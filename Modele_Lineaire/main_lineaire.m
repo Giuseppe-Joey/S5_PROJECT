ee29fef2e8972c645d499caf09b48939a8a2770a
vinceroy9 <vincent_roy1997@hotmail.com>
2023-03-05 21:12:13 -0500

    presque fini matrices

--- Modele_Lineaire/main_lineaire.m
+++ Modele_Lineaire/main_lineaire.m
@@ -15,10 +15,10 @@ Ayeq = 0;               %en degres
 Pzeq = .015;            %en metres
 
 %% Exemple de trajectoire
-t_des     = [0:1:40]';
-% x_des     = [t_des, [0 0 0.5 1  0 -1 0 1 0]'*0.05];
-% y_des     = [t_des, [0 0 0 0 -1  0 1 0 0]'*0.05];
-% z_des     = [t_des, [1 1 1 1  1  1 1 1 1]'*.015];
+t_des     = [0:1:8]';
+x_des     = [t_des, [0 0 0.5 1  0 -1 0 1 0]'*0.05];
+y_des     = [t_des, [0 0 0 0 -1  0 1 0 0]'*0.05];
+z_des     = [t_des, [1 1 1 1  1  1 1 1 1]'*.015];
 tfin = 50;
 
 %% Initialisation constantes
@@ -30,22 +30,16 @@ constantes_lineaire % call le fichier des constantes
 % VB = [t_des, (linspace(0, -Vmax, length(t_des)))'];
 % VC = [t_des, (linspace(0, -Vmax, length(t_des)))'];
 
-% Pour l'instant on va mettre des tensions fixes et regarder la sortie
-% On pourra tester les modeles lineaire avec le banc d'essai du prof
-% J'ai mis des valeurs arbiraires juste pour pouvoir tester sans avoir de 0
-VA_eq = -2.2; %[V]
-VB_eq = -2.1; %[V]
-VC_eq = -2.3; %[V]
-
 %% Calcul des compensateurs
 %iniCTL_ver4    %Calculez vos compensateurs ici
 
-
 %% Linéarisation
 % Constantes à l'équilibre 
-ib_eq = VB_eq/RB;
-ic_eq = VC_eq/RC;
-z_eq = Pzeq;        %Pzeq est une variable globale declaree plus haut
+V_eq = 5; % Chiffre arbitraire c'est À CALCULER PAR JOEY
+ia_eq = V_eq/RA;
+ib_eq = V_eq/RB;
+ic_eq = V_eq/RC;
+z_eq = Pzeq;       %Pzeq est une variable globale declaree plus haut
 
 % Parties utilisées souvents dans les dérivées partielles
 den_fe = ae0 + ae1*z_eq + ae2*z_eq^2 + ae3*z_eq^3;
@@ -54,11 +48,18 @@ diff_den_fe = ae1 + 2*ae2*z_eq + 3*ae3*z_eq^2;
 den_fs = as0 + as1*z_eq + as2*z_eq^2 + as3*z_eq^3;
 diff_den_fs = as1 + 2*as2*z_eq + 3*as3*z_eq^2;
 
+num_fe_a = ia_eq*abs(ia_eq) + be1*ia_eq;
+diff_num_fe_a= (2*ia_eq^2 + be1*abs(ia_eq)) / abs(ia_eq);
+
 num_fe_b = ib_eq*abs(ib_eq) + be1*ib_eq;
 diff_num_fe_b = (2*ib_eq^2 + be1*abs(ib_eq)) / abs(ib_eq);
 
 num_fe_c = ic_eq*abs(ic_eq) + be1*ic_eq;
 diff_num_fe_c = (2*ic_eq^2 + be1*abs(ic_eq)) / abs(ic_eq);
+
+diff_fa_za = (-num_fe_a*diff_den_fe)/(den_fe^2) + diff_den_fs/(den_fs)^2;
+diff_fb_zb = (-num_fe_b*diff_den_fe)/(den_fe^2) + diff_den_fs/(den_fs)^2;
+diff_fc_zc = (-num_fe_c*diff_den_fe)/(den_fe^2) + diff_den_fs/(den_fs)^2;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                  Dérivées partielles delta z_dot_dot                    %
